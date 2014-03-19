f4ce4803152b696011516e50991d315d79185271
cwoodford <chase@chasewoodford.com>
2014-03-18 20:48:13 -0400

    begin build of site footer

--- wp-content/themes/starkers-master/parts/shared/footer.php
+++ wp-content/themes/starkers-master/parts/shared/footer.php
@@ -1,4 +1,9 @@
-	
+    <section class="site-footer-links-container">
+        <a href="#" title=""><span class="icon-subscribe"></span></a>
+        <a href="#" title=""><span class="icon-rss"></span></a>
+        <a href="#" title=""><span class="icon-contact"></span></a>
+        <a href="#" title=""><span class="icon-feedback"></span></a>
+    </section>
 	<footer>
 		&copy; <?php echo date("Y"); ?> <?php bloginfo( 'name' ); ?>. All rights reserved.
 	</footer>
