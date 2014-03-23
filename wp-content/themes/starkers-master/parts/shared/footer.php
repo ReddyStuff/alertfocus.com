44a583c0af10130ae598db0dd49c272147aeaef5
cwoodford <chase@chasewoodford.com>
2014-03-23 18:18:28 -0400

    finishing footer

--- wp-content/themes/starkers-master/parts/shared/footer.php
+++ wp-content/themes/starkers-master/parts/shared/footer.php
@@ -1,29 +1,39 @@
-    <section class="site-footer-links-container">
-        <div class="site-footer-link-container">
-            <a class="site-footer-link" href="#" title="">
+    <section class="page-footer-links-container">
+        <div class="page-footer-link-container">
+            <a class="page-footer-link" href="#" title="">
                 <span class="icon-subscribe"></span>
-                <div class="site-footer-link-text">Subscribe</div>
+                <div class="page-footer-link-text">Subscribe</div>
             </a>
         </div>
-        <div class="site-footer-link-container">
-            <a class="site-footer-link" href="<?php bloginfo('rss_url'); ?>" title="RSS Feed">
+        <div class="page-footer-link-container">
+            <a class="page-footer-link" href="<?php bloginfo('rss_url'); ?>" title="RSS Feed">
                 <span class="icon-rss"></span>
-                <div class="site-footer-link-text">RSS</div>
+                <div class="page-footer-link-text">RSS</div>
             </a>
         </div>
-        <div class="site-footer-link-container">
-            <a class="site-footer-link" href="mailto:gary@garyames.net" title="Contact Gary Ames">
+        <div class="page-footer-link-container">
+            <a class="page-footer-link" href="mailto:gary@garyames.net" title="Contact Gary Ames">
                 <span class="icon-contact"></span>
-                <div class="site-footer-link-text">Contact</div>
+                <div class="page-footer-link-text">Contact</div>
             </a>
         </div>
-        <div class="site-footer-link-container">
-            <a class="site-footer-link" href="#" title="">
+        <div class="page-footer-link-container">
+            <a class="page-footer-link" href="#" title="">
                 <span class="icon-feedback"></span>
-                <div class="site-footer-link-text">Feedback</div>
+                <div class="page-footer-link-text">Feedback</div>
             </a>
         </div>
     </section>
 	<footer>
-		&copy; <?php echo date("Y"); ?> <?php bloginfo( 'name' ); ?>. All rights reserved.
+
+        <section class="site-footer-container">
+            <div class="site-footer-bar">
+                <span class="site-footer-bar-border"></span>
+
+                <div class="site-footer-text">
+                    &copy; <?php echo date("Y"); ?> <?php bloginfo( 'name' ); ?>. All rights reserved.
+                </div>
+            </div>
+        </section>
+
 	</footer>
