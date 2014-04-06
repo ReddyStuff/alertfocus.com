ba0905e623b2c07541d0d9019c78a439b39adea1
cwoodford <chase@chasewoodford.com>
2014-04-05 21:49:17 -0400

    minor adjustment to viewport

--- wp-content/themes/starkers-master/parts/html-header.php
+++ wp-content/themes/starkers-master/parts/html-header.php
@@ -8,7 +8,7 @@
 		<title><?php bloginfo( 'name' ); ?><?php wp_title( '|' ); ?></title>
         <meta charset="utf-8">
 	  	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="viewport" content="width=980">
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 		<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon.ico"/>
 		<?php wp_head(); ?>
