1668267c3c747bd02fba70da0215e99cf8045484
cwoodford <chase@chasewoodford.com>
2014-04-06 18:08:24 -0400

    adding comments to post-thumbnail feature

--- wp-content/themes/starkers-master/parts/post-thumbnail.php
+++ wp-content/themes/starkers-master/parts/post-thumbnail.php
@@ -1,5 +1,14 @@
+<?php
+/**
+ * The template for displaying thumbnails for posts.
+ *
+ * If post has a featured image set, display it. Otherwise, pull a low-res image
+ * from lorempixel.com and use that.
+ */
+?>
+
 <?php if( has_post_thumbnail() ) { ?>
   <?php the_post_thumbnail();?>
 <?php } else { ?>
-  <img src="http://lorempixel.com/200/220" alt="stock image"/>
+  <img src="http://lorempixel.com/200/210" alt="Stock image from lorempixel.com"/>
 <?php } ?>
