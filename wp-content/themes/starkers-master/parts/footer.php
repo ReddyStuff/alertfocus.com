fe2af0070d020c558a14ba64fe8742c90ae3d16a
cwoodford <chase@chasewoodford.com>
2014-04-22 21:37:28 -0400

    re-built footer contact bar

--- wp-content/themes/starkers-master/parts/footer.php
+++ wp-content/themes/starkers-master/parts/footer.php
@@ -1,28 +1,25 @@
     <section class="page-footer-links-container">
-<!--        <div class="page-footer-link-container">-->
-<!--            <a class="page-footer-link" href="#" title="">-->
-<!--                <span class="icon-subscribe"></span>-->
-<!--                <div class="page-footer-link-text">Subscribe</div>-->
-<!--            </a>-->
-<!--        </div>-->
-<!--        <div class="page-footer-link-container">-->
-<!--            <a class="page-footer-link" href="--><?php //bloginfo('rss_url'); ?><!--" title="RSS Feed">-->
-<!--                <span class="icon-rss"></span>-->
-<!--                <div class="page-footer-link-text">RSS</div>-->
-<!--            </a>-->
-<!--        </div>-->
-<!--        <div class="page-footer-link-container">-->
-<!--            <a class="page-footer-link" href="mailto:garyames@alertfocus.com" title="Contact Gary Ames">-->
-<!--                <span class="icon-contact"></span>-->
-<!--                <div class="page-footer-link-text">Contact</div>-->
-<!--            </a>-->
-<!--        </div>-->
-<!--        <div class="page-footer-link-container">-->
-<!--            <a class="page-footer-link" href="mailto:admin@alertfocus.com" title="Leave Feedback for Site Admins">-->
-<!--                <span class="icon-feedback"></span>-->
-<!--                <div class="page-footer-link-text">Feedback</div>-->
-<!--            </a>-->
-<!--        </div>-->
+        <div class="page-footer-link-container">
+            <a class="page-footer-link" href="https://goo.gl/maps/92FMt" target="_blank" title="28 ROCK HILL ROAD, BALA CYNWYD, PA 19004">
+                <span class="icon-address"></span>
+                <div class="page-footer-link-text">Address:</div>
+                <div class="page-footer-link-text">28 ROCK HILL ROAD<br/>BALA CYNWYD, PA 19004</div>
+            </a>
+        </div>
+        <div class="page-footer-link-container">
+            <a class="page-footer-link" href="mailto:garyames@alertfocus.com" title="garyames@alertfocus.com">
+                <span class="icon-email"></span>
+                <div class="page-footer-link-text">Email:</div>
+                <div class="page-footer-link-text">garyames@alertfocus.com</div>
+            </a>
+        </div>
+        <div class="page-footer-link-container">
+            <a class="page-footer-link" href="tel:1-610-668-3223" title="(610) 668-3223">
+                <span class="icon-call"></span>
+                <div class="page-footer-link-text">Phone:</div>
+                <div class="page-footer-link-text">(610) 668-3223</div>
+            </a>
+        </div>
     </section>
 	<footer>
 
