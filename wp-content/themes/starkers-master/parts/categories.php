76b4766c1d0dd7594a1c50b9156d7841c65aed6a
cwoodford <chase@chasewoodford.com>
2014-04-27 19:40:17 -0400

    update to related articles section

--- wp-content/themes/starkers-master/parts/categories.php
+++ wp-content/themes/starkers-master/parts/categories.php
@@ -1,9 +1,22 @@
 <?php
-$argsCategories = array(
+$argsCategoriesRelieve = array(
     'orderby' => 'name',
-    'order' => 'ASC'
+    'order' => 'ASC',
+    'parent' => 2
 );
-$categories = get_categories($argsCategories);
+$argsCategoriesPerform = array(
+    'orderby' => 'name',
+    'order' => 'ASC',
+    'parent' => 3
+);
+$argsCategoriesOther = array(
+    'orderby' => 'name',
+    'order' => 'ASC',
+    'include' => '17,18,19,20'
+);
+$categoriesRelieve = get_categories($argsCategoriesRelieve);
+$categoriesPerform = get_categories($argsCategoriesPerform);
+$categoriesOther = get_categories($argsCategoriesOther);
 ?>
 
 <div class="categories-container">
@@ -12,11 +25,49 @@ $categories = get_categories($argsCategories);
     </div>
     <div class="categories">
         <ul class="categories-list">
-        <?php foreach($categories as $category) { echo '
-            <li class="categories-list-item">
-                <a class="categories-list-anchor"
-                    href="' . get_category_link( $category->term_id ) . '"
-                    title="' . $category->name . '">'
+
+            <?php $category_relieve = get_category_link( 2 ); ?>
+            <li class="categories-list-item full">
+                <a class="categories-list-anchor" href="<?php echo esc_url( $category_relieve ); ?>" title="Relieve">Relieve</a>
+            </li>
+
+            <?php foreach($categoriesRelieve as $category) { echo '
+                <li class="categories-list-item partial">
+                    <a class="categories-list-anchor"
+                        href="' . get_category_link( $category->term_id ) . '"
+                        title="' . $category->name . '">'
+                    . $category->name .
+                    '</a>
+                </li>
+            ';} ?>
+
+            <li class="categories-list-item-empty"></li>
+
+            <?php $category_perform = get_category_link( 3 ); ?>
+            <li class="categories-list-item full">
+                <a class="categories-list-anchor" href="<?php echo esc_url( $category_perform ); ?>" title="Perform">Perform</a>
+            </li>
+
+            <?php foreach($categoriesPerform as $category) { echo '
+                <li class="categories-list-item partial">
+                    <a class="categories-list-anchor"
+                        href="' . get_category_link( $category->term_id ) . '"
+                        title="' . $category->name . '">'
+                    . $category->name .
+                    '</a>
+                </li>
+            ';} ?>
+
+            <li class="categories-list-item-empty"></li>
+
+            <li class="categories-list-item full">
+                <span class="categories-list-anchor">Other</span>
+            </li>
+            <?php foreach($categoriesOther as $category) { echo '
+                <li class="categories-list-item partial">
+                    <a class="categories-list-anchor"
+                        href="' . get_category_link( $category->term_id ) . '"
+                        title="' . $category->name . '">'
                 . $category->name .
                 '</a>
             </li>
