6b74d69dce7f0c6bd509cc6cd5704af2c7695bf3
cwoodford <chase@chasewoodford.com>
2014-04-28 19:26:50 -0400

    adding Google Analytics code

--- wp-content/themes/starkers-master/parts/html-header.php
+++ wp-content/themes/starkers-master/parts/html-header.php
@@ -4,13 +4,14 @@
 <!--[if IE 7 ]><html class="no-js ie7" lang="en"><![endif]--> 
 <!--[if IE 8 ]><html class="no-js ie8" lang="en"><![endif]--> 
 <!--[if (gte IE 9)|(gt IEMobile 7)|!(IEMobile)|!(IE)]><!--><html class="no-js" lang="en"><!--<![endif]-->
-	<head>
-		<title><?php bloginfo( 'name' ); ?><?php wp_title( '|' ); ?></title>
-        <meta charset="utf-8">
-	  	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-		<meta name="viewport" content="width=980">
-		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-		<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon.ico"/>
-		<?php wp_head(); ?>
-	</head>
-	<body <?php body_class(); ?>>
+<head>
+    <title><?php bloginfo( 'name' ); ?><?php wp_title( '|' ); ?></title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=980">
+    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+    <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon.ico"/>
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+<?php include_once("../analyticstracking.php") ?>
