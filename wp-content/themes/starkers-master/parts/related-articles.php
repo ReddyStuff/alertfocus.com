78b92f06097db2c42f3543c7f151654e7e7df4c3
cwoodford <chase@chasewoodford.com>
2014-09-20 11:39:16 -0400

    minor code cleanup

--- wp-content/themes/starkers-master/parts/related-articles.php
+++ wp-content/themes/starkers-master/parts/related-articles.php
@@ -5,14 +5,14 @@
     <div class="related-articles">
         <ul class="related-article-list">
             <?php while ( have_posts() ) : the_post(); echo '
-                    <li class="hero-list-item">
-                        <a class="hero-list-anchor"
-                            href="' . get_permalink() . '"
-                            title="' . get_the_title() .'">'
-                . get_the_title() .
-                '</a>
-            </li>
-        '; endwhile; ?>
+                <li class="hero-list-item">
+                    <a class="hero-list-anchor"
+                        href="' . get_permalink() . '"
+                        title="' . get_the_title() .'">'
+                        . get_the_title() .
+                    '</a>
+                </li>
+            '; endwhile; ?>
         </ul>
         <div class="clearfix"></div>
     </div>
